19cf861b4f0fc98137d76dcca25f882a040e6d25
Phil Everson <peverson@klick.com>
2014-12-09 08:24:57 -0500

    Icons and basic pass layout

--- app/index.php
+++ app/index.php
@@ -1,13 +1,6 @@
 <?php
 require('PKPass.php');
 
-//$tempQ = $GET_['key'];
-
-
-
-//$url = 'http://zxing.org/w/decode?u=http://picpaste.com/pics/viatest-yNt4taEn.1417981434.jpg';
-// $url = 'http://zxing.org/w/decode?u=http://picpaste.com/pics/viatest_65-HxIpqYvM.1417981361.jpg';
-
 $url = 'http://zxing.org/w/decode?u=http://oi60.tinypic.com/4hval3.jpg';
 
 // using file_get_contents function
@@ -18,97 +11,105 @@ $arrowParse = explode('>',$preParse[1]);
 
 //the content of the code that's returned from zxing.org 
 $rawTextParsed = $arrowParse[1];
-//echo $rawTextParsed;
 
+if($rawTextParsed != null)
+{
+    $pass = new PKPass\PKPass();
 
+    $pass->setCertificate('Cert-viaporter-1.p12');  // 2. Set the path to your Pass Certificate (.p12 file)
+    $pass->setCertificatePassword('Philip99');     // 2. Set password for certificate
+    $pass->setWWDRcertPath('AppleWWDRCA.pem'); // 3. Set the path to your WWDR Intermediate certificate (.pem file)
 
-
-
-
-
-
-$pass = new PKPass\PKPass();
-
-$pass->setCertificate('Cert-viaporter-1.p12');  // 2. Set the path to your Pass Certificate (.p12 file)
-$pass->setCertificatePassword('Philip99');     // 2. Set password for certificate
-$pass->setWWDRcertPath('AppleWWDRCA.pem'); // 3. Set the path to your WWDR Intermediate certificate (.pem file)
-
-// Top-Level Keys http://developer.apple.com/library/ios/#documentation/userexperience/Reference/PassKit_Bundle/Chapters/TopLevel.html
-$standardKeys         = array(
-    'description'        => 'Demo pass',
-    'formatVersion'      => 1,
-	'authenticationToken' => '1234567890abcdef1234',
-    'organizationName'   => 'Flight Express',
-    'passTypeIdentifier' => 'pass.via-porter', // 4. Set to yours
-    'serialNumber'       => '123456',
-    'teamIdentifier'     => 'GH2A55GQ4M'           // 4. Set to yours
-);
-$associatedAppKeys    = array();
-$relevanceKeys        = array();
-$styleKeys            = array(
-    'boardingPass' => array(
-        'primaryFields' => array(
-            array(
-                'key'   => 'origin',
-                'label' => 'San Francisco',
-                'value' => 'SFO'
+    // Top-Level Keys http://developer.apple.com/library/ios/#documentation/userexperience/Reference/PassKit_Bundle/Chapters/TopLevel.html
+    $standardKeys         = array(
+        'description'        => 'Demo pass',
+        'formatVersion'      => 1,
+    	'authenticationToken' => '1234567890abcdef1234',
+        'organizationName'   => 'Flight Express',
+        'passTypeIdentifier' => 'pass.via-porter', // 4. Set to yours
+        'serialNumber'       => '123456',
+        'teamIdentifier'     => 'GH2A55GQ4M'           // 4. Set to yours
+    );
+    $associatedAppKeys    = array();
+    $relevanceKeys        = array();
+    $styleKeys            = array(
+        'boardingPass' => array(
+            'primaryFields' => array(
+                array(
+                    'key'   => 'origin',
+                    'label' => 'Departing',
+                    'value' => 'TORONTO'
+                ),
+                array(
+                    'key'   => 'destination',
+                    'label' => 'Ariving',
+                    'value' => 'CORNWALL'
+                )
             ),
-            array(
-                'key'   => 'destination',
-                'label' => 'London',
-                'value' => 'LHR'
-            )
-        ),
-        'secondaryFields' => array(
-            array(
-                'key'   => 'gate',
-                'label' => 'Gate',
-                'value' => 'F13'
+            'auxiliaryFields' => array(
+                array(
+                    'key'   => 'depart-date-time',
+                    'label' => '',
+                    'value' => '07/11/2012 10:22'
+                ),
+                array(
+                    'key'   => 'arival-date-time',
+                    'label' => '',
+                    'value' => '07/11/2012 10:22'
+                )
             ),
-            array(
-                'key'   => 'date',
-                'label' => 'Departure date',
-                'value' => '07/11/2012 10:22'
-            )
+            'headerFields' => array(
+                array(
+                    'key'   => 'trainNum',
+                    'label' => 'Train',
+                    'value' => '48'
+                )
+            ),
+            'backFields' => array(
+                array(
+                    'key'   => 'passenger-name',
+                    'label' => 'Passenger',
+                    'value' => 'Phil Everson'
+                )
+            ),
+            'transitType' => 'PKTransitTypeTrain'
+        )
+    );
+    $visualAppearanceKeys = array(
+        'barcode'         => array(
+            'format'          => 'PKBarcodeFormatAztec',
+            'message'         =>  $rawTextParsed,
+            'messageEncoding' => 'iso-8859-1'
         ),
-        'backFields' => array(
-            array(
-                'key'   => 'passenger-name',
-                'label' => 'Passenger',
-                'value' => 'Phil Everson'
-            )
-        ),
-        'transitType' => 'PKTransitTypeAir'
-    )
-);
-$visualAppearanceKeys = array(
-    'barcode'         => array(
-        'format'          => 'PKBarcodeFormatAztec',
-        'message'         =>  $rawTextParsed, //=> '1811201424352Everson                       3   9C CWLLTRTOVIA65  201412071305Phil                P1N YTHEHQ81020141118124602ES NB'//,
-       'messageEncoding' => 'iso-8859-1'
-    ),
-    'backgroundColor' => 'rgb(107,156,196)',
-    'logoText'        => 'Flight info'
-);
-$webServiceKeys       = array();
+        'backgroundColor' => 'rgb(89,80,81)',
+        'foregroundColor' => 'rgb(255,255,255)',
+        'labelColor'      => 'rgb(255,255,255)'
+    );
+    $webServiceKeys       = array();
 
-// Merge all pass data and set JSON for $pass object
-$passData = array_merge(
-    $standardKeys,
-    $associatedAppKeys,
-    $relevanceKeys,
-    $styleKeys,
-    $visualAppearanceKeys,
-    $webServiceKeys
-);
+    // Merge all pass data and set JSON for $pass object
+    $passData = array_merge(
+        $standardKeys,
+        $associatedAppKeys,
+        $relevanceKeys,
+        $styleKeys,
+        $visualAppearanceKeys,
+        $webServiceKeys
+    );
 
-$pass->setJSON(json_encode($passData));
+    $pass->setJSON(json_encode($passData));
 
-// Add files to the PKPass package
-$pass->addFile('images/icon.png');
-$pass->addFile('images/icon@2x.png');
-$pass->addFile('images/logo.png');
+    // Add files to the PKPass package
+    $pass->addFile('images/icon.png');
+    $pass->addFile('images/icon@2x.png');
+    $pass->addFile('images/logo.png');
+    $pass->addFile('images/logo@2x.png');
 
-if(!$pass->create(true)) { // Create and output the PKPass
-    echo 'Error: '.$pass->getError();
+    if(!$pass->create(true)) { // Create and output the PKPass
+        echo 'Error: '.$pass->getError();
+    }
+}
+else
+{
+    echo 'problem generating pass';
 }
