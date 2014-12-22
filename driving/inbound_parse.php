323317bddd84c2b7a4d727dc64da65a9c602bdb6
Phil Everson <peverson@klick.com>
2014-12-22 11:12:14 -0500

    No more postmarkapp library thingy...

--- driving/inbound_parse.php
+++ driving/inbound_parse.php
@@ -1,16 +1,16 @@
 <?php
 
-require_once 'postmark-inbound-php-master/lib/Postmark/Autoloader.php';
-\Postmark\Autoloader::register();
+// require_once 'postmark-inbound-php-master/lib/Postmark/Autoloader.php';
+// \Postmark\Autoloader::register();
 
-// this file should be the target of the callback you set in your postmark account
-$inbound = new \Postmark\Inbound(file_get_contents('inbound.json'));
-//$inbound = new \Postmark\Inbound(file_get_contents('php://input'));
+// // this file should be the target of the callback you set in your postmark account
+// $inbound = new \Postmark\Inbound(file_get_contents('inbound.json'));
+// //$inbound = new \Postmark\Inbound(file_get_contents('php://input'));
 
-echo $inbound->Subject();
-echo $inbound->FromEmail();
+// echo $inbound->Subject();
+// echo $inbound->FromEmail();
 
-echo 'request bin now...';
+// echo 'request bin now...';
 
 $result = file_get_contents('http://requestb.in/1lqp0sw1');
 echo $result;
