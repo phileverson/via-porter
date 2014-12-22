f3ffafb201796acab8d456a4d46b6cb6a619f04f
Phil Everson <peverson@klick.com>
2014-12-22 10:59:43 -0500

    Adding sample PHP parsing stuff

--- driving/inbound_parse.php
+++ driving/inbound_parse.php
@@ -1,4 +1,18 @@
 <?php
-    $result = file_get_contents('http://requestb.in/1lqp0sw1');
-    echo $result;
+
+require_once 'postmark-inbound-php-master/lib/Postmark/Autoloader.php';
+\Postmark\Autoloader::register();
+
+// this file should be the target of the callback you set in your postmark account
+$inbound = new \Postmark\Inbound(file_get_contents('inbound.json'));
+//$inbound = new \Postmark\Inbound(file_get_contents('php://input'));
+
+echo $inbound->Subject();
+echo $inbound->FromEmail();
+
+echo 'request bin now...';
+
+$result = file_get_contents('http://requestb.in/1lqp0sw1');
+echo $result;
+
 ?>
