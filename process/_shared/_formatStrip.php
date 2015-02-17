25d35968b43b7d5e59de821b2607a052f18f4162
Phil Everson <phil@phileverson.com>
2015-02-16 20:47:58 -0500

    Cleaned up formatStrip and added catch on attachment length to the get strips function... (should fix iOS bug)

--- process/_shared/_formatStrip.php
+++ process/_shared/_formatStrip.php
@@ -1,24 +1,4 @@
 <?php
-function curl_file_get_contents($url)
-{
- $curl = curl_init();
- $userAgent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)';
- 
- curl_setopt($curl,CURLOPT_URL,$url); //The URL to fetch. This can also be set when initializing a session with curl_init().
- curl_setopt($curl,CURLOPT_RETURNTRANSFER,TRUE); //TRUE to return the transfer as a string of the return value of curl_exec() instead of outputting it out directly.
- curl_setopt($curl,CURLOPT_CONNECTTIMEOUT,5); //The number of seconds to wait while trying to connect.	
- 
- curl_setopt($curl, CURLOPT_USERAGENT, $userAgent); //The contents of the "User-Agent: " header to be used in a HTTP request.
- curl_setopt($curl, CURLOPT_FAILONERROR, TRUE); //To fail silently if the HTTP code returned is greater than or equal to 400.
- curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE); //To follow any "Location: " header that the server sends as part of the HTTP header.
- curl_setopt($curl, CURLOPT_AUTOREFERER, TRUE); //To automatically set the Referer: field in requests where it follows a Location: redirect.
- curl_setopt($curl, CURLOPT_TIMEOUT, 10); //The maximum number of seconds to allow cURL functions to execute.	
- 
- $contents = curl_exec($curl);
- curl_close($curl);
- return $contents;
-}
-
 
 function formatStrip($ourPassID, $i_passSet) {
 
--- process/_shared/_getStrips.php
+++ process/_shared/_getStrips.php
@@ -7,11 +7,14 @@ function getStrips($ourPassID, $postMarkInbound) {
 	foreach($postMarkInbound->Attachments() as $attachment) {
 	    if(strpos($attachment->Name,'jpg') !== false)
 	    {
-	    	$data = base64_decode($attachment->Content);
-	    	mkdir('_passes/' . $ourPassID . '/' . $passCount);
-	    	file_put_contents('_passes/' . $ourPassID . '/' . $passCount . '/strip.png', $data);
-	    	$barcodes[$passCount] = 'strip.png';
-	    	$passCount = $passCount + 1;
+	    	if(strlen($attachment->Name) == 29)
+	    	{
+		    	$data = base64_decode($attachment->Content);
+		    	mkdir('_passes/' . $ourPassID . '/' . $passCount);
+		    	file_put_contents('_passes/' . $ourPassID . '/' . $passCount . '/strip.png', $data);
+		    	$barcodes[$passCount] = 'strip.png';
+		    	$passCount = $passCount + 1;
+	    	}
 	    }
 	}
 
